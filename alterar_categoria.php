0f3210006b863f46805f8f563056a219428d1060
vanessaborges123 <96017591+vanessaborges123@users.noreply.github.com>
2026-05-05 21:47:15 -0300

    Update alterar_categoria.php

--- alterar_categoria.php
+++ alterar_categoria.php
@@ -1,15 +1,44 @@
 <?php
     require_once('cabecalho.php');
+    require_once('conexao.php');
+    $mensagem = "";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $nome = $_POST['descricao'];
+        $id = $_GET['id'];
+        try{
+          $sql = "UPDATE categoria SET nome = ? WHERE id = ?";
+          $stmt = $pdo->prepare($sql);
+          if($stmt->execute([$nome, $id])){
+            $mensagem = "<p>Alteração realizada!</p>";
+          } else {
+            $mensagem = "<p>Erro ao alterar! Tente novamente</p>";
+          }
+        } catch(Exception $e){
+          echo "Erro: ".$e->getMessage();
+        }
+      }
+    try{
+        $stmt = 
+            $pdo->prepare("SELECT * from categoria WHERE id = ?");
+        $stmt->execute([$_GET['id']]);
+        $resultado = $stmt->fetch();
+    } catch (Exception $e){
+        echo "Erro: ".$e->getMessage();
+    }
 ?>
 
 <h1>Alterar Categoria</h1>
-    <form method="post">
+    <form method="post" 
+        action="alterar_categoria.php?id=<?= $resultado['id']?>">
         <div class="mb-3">
-              <label for="descricao" class="form-label">Informe a descrição</label>
-              <input type="text" id="descricao" name="descricao" class="form-control" required="">
+            <label for="descricao" class="form-label">Informe a descrição</label>
+            <input value="<?= $resultado['nome']?>" type="text" id="descricao" name="descricao" class="form-control" required="">
         </div>
         <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
+    <?php
+      echo $mensagem;
+    ?>
 
 <?php
     require_once('rodape.php');
